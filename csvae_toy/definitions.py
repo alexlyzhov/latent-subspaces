e105909fac6fbd1e63e9802700f1da2897005742
nikkou <nikkou.postbox@gmail.com>
2019-10-23 20:03:35 +0300

    Move full CSVAE definition to scripts

--- csvae_toy/definitions.py
+++ csvae_toy/definitions.py
@@ -320,3 +320,166 @@ class CSVAE_without_delta_net(nn.Module):
         std = logvar.mul(0.5).exp_()
         eps = torch.FloatTensor(std.size()).normal_().to(mu.device)
         return eps.mul(std).add_(mu)
+    
+    
+class CSVAE(nn.Module):
+    def __init__(self, input_dim, labels_dim, z_dim, w_dim):
+        super(CSVAE, self).__init__()
+        self.input_dim = input_dim
+        self.labels_dim = labels_dim
+        self.z_dim = z_dim
+        self.w_dim = w_dim
+        
+        self.encoder_xy_to_w = nn.Sequential(nn.Linear(input_dim+labels_dim, w_dim), nn.ReLU(), nn.Linear(w_dim, w_dim), nn.ReLU())
+        self.mu_xy_to_w = nn.Linear(w_dim, w_dim)
+        self.logvar_xy_to_w = nn.Linear(w_dim, w_dim)
+        
+        self.encoder_x_to_z = nn.Sequential(nn.Linear(input_dim, z_dim), nn.ReLU(), nn.Linear(z_dim, z_dim), nn.ReLU())
+        self.mu_x_to_z = nn.Linear(z_dim, z_dim)
+        self.logvar_x_to_z = nn.Linear(z_dim, z_dim)
+        
+        self.encoder_y_to_w = nn.Sequential(nn.Linear(labels_dim, w_dim), nn.ReLU(), nn.Linear(w_dim, w_dim), nn.ReLU())
+        self.mu_y_to_w = nn.Linear(w_dim, w_dim)
+        self.logvar_y_to_w = nn.Linear(w_dim, w_dim)
+        
+        # Add sigmoid or smth for images!
+        self.decoder_zw_to_x = nn.Sequential(nn.Linear(z_dim+w_dim, z_dim+w_dim), nn.ReLU(), nn.Linear(z_dim+w_dim, z_dim+w_dim), nn.ReLU())
+        self.mu_zw_to_x = nn.Linear(z_dim+w_dim, input_dim)
+        self.logvar_zw_to_x = nn.Linear(z_dim+w_dim, input_dim)
+        
+        self.decoder_z_to_y = nn.Sequential(nn.Linear(z_dim, z_dim), nn.ReLU(), nn.Linear(z_dim, z_dim), nn.ReLU(),
+                                            nn.Linear(z_dim, labels_dim), nn.Sigmoid())
+
+        self.init_params()
+
+    def init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.xavier_normal_(m.weight.data)
+        
+    def q_zw(self, x, y):
+        """
+        VARIATIONAL POSTERIOR
+        :param x: input image
+        :return: parameters of q(z|x), (MB, hid_dim)
+        """
+        xy = torch.cat([x, y], dim=1)
+        
+        intermediate = self.encoder_x_to_z(x)
+        z_mu = self.mu_x_to_z(intermediate)
+        z_logvar = self.mu_x_to_z(intermediate)
+        
+        intermediate = self.encoder_xy_to_w(xy)
+        w_mu_encoder = self.mu_xy_to_w(intermediate)
+        w_logvar_encoder = self.mu_xy_to_w(intermediate)
+        
+        intermediate = self.encoder_y_to_w(y)
+        w_mu_prior = self.mu_y_to_w(intermediate)
+        w_logvar_prior = self.mu_y_to_w(intermediate)
+        
+        return w_mu_encoder, w_logvar_encoder, w_mu_prior, \
+               w_logvar_prior, z_mu, z_logvar
+    
+    def p_x(self, z, w):
+        """
+        GENERATIVE DISTRIBUTION
+        :param z: latent vector          (MB, hid_dim)
+        :return: parameters of p(x|z)    (MB, inp_dim)
+        """
+        
+        zw = torch.cat([z, w], dim=1)
+        
+        intermediate = self.decoder_zw_to_x(zw)
+        mu = self.mu_zw_to_x(intermediate)
+        logvar = self.logvar_zw_to_x(intermediate)
+        
+        return mu, logvar
+
+    def forward(self, x, y):
+        """
+        Encode the image, sample z and decode 
+        :param x: input image
+        :return: parameters of p(x|z_hat), z_hat, parameters of q(z|x)
+        """
+        w_mu_encoder, w_logvar_encoder, w_mu_prior, \
+            w_logvar_prior, z_mu, z_logvar = self.q_zw(x, y)
+        w_encoder = self.reparameterize(w_mu_encoder, w_logvar_encoder)
+        w_prior = self.reparameterize(w_mu_prior, w_logvar_prior)
+        z = self.reparameterize(z_mu, z_logvar)
+        zw = torch.cat([z, w_encoder], dim=1)
+        
+        x_mu, x_logvar = self.p_x(z, w_encoder)
+        y_pred = self.decoder_z_to_y(z)
+        
+        return x_mu, x_logvar, zw, y_pred, \
+               w_mu_encoder, w_logvar_encoder, w_mu_prior, \
+               w_logvar_prior, z_mu, z_logvar
+
+    def calculate_loss(self, x, y):
+        """
+        Given the input batch, compute the negative ELBO 
+        :param x:   (MB, inp_dim)
+        :param beta: Float
+        :param average: Compute average over mini batch or not, bool
+        :return: -RE + beta * KL  (MB, ) or (1, )
+        """
+        x_mu, x_logvar, zw, y_pred, \
+            w_mu_encoder, w_logvar_encoder, w_mu_prior, \
+            w_logvar_prior, z_mu, z_logvar = self.forward(x, y)
+        
+        x_recon = nn.MSELoss()(x_mu, x)
+        
+        w_dist = dists.MultivariateNormal(w_mu_encoder.flatten(), torch.diag(w_logvar_encoder.flatten().exp()))
+        w_prior = dists.MultivariateNormal(w_mu_prior.flatten(), torch.diag(w_logvar_prior.flatten().exp()))
+        w_kl = dists.kl.kl_divergence(w_dist, w_prior)
+        
+        z_dist = dists.MultivariateNormal(z_mu.flatten(), torch.diag(z_logvar.flatten().exp()))
+        z_prior = dists.MultivariateNormal(torch.zeros(self.z_dim * z_mu.size()[0]).cuda(), torch.eye(self.z_dim * z_mu.size()[0]).cuda())
+        z_kl = dists.kl.kl_divergence(z_dist, z_prior)
+        
+        y_pred_negentropy = (y_pred.log() * y_pred + (1-y_pred).log() * (1-y_pred)).mean()
+
+        y_recon = nn.BCELoss()(y_pred, y)
+        # alternatively use predicted logvar too to evaluate density of input
+        
+        ELBO = 20 * x_recon + 0.2 * z_kl + 1 * w_kl + 10 * y_pred_negentropy + 1 * y_recon
+        
+        return ELBO, x_recon, w_kl, z_kl, y_pred_negentropy, y_recon
+
+#     def reconstruct_x(self, x, y):
+#         x_mean, _, _, _, _ = self.forward(x, y)
+#         return x_mean
+
+#     def calculate_nll(self, X, samples=5000):
+#         """
+#         Estimate NLL by importance sampling
+#         :param X: dataset, (N, inp_dim)
+#         :param samples: Samples per observation
+#         :return: IS estimate
+#         """   
+#         prob_sum = 0.
+
+#         for i in range(samples):
+#             KL, RE, _ = self.calculate_loss(X)
+#             prob_sum += (KL + RE).exp_()
+            
+#         return - (prob_sum / samples).sum().log_()
+
+#     def generate_x(self, N=25):
+#         """
+#         Sample, using you VAE: sample z from prior and decode it 
+#         :param N: number of samples
+#         :return: X (N, inp_size)
+#         """
+
+#         m = MultivariateNormal(torch.zeros(self.z_dim + self.w_dim), torch.eye(self.z_dim + self.w_dim))
+#         z = m.sample(sample_shape=torch.Size([N])) 
+        
+#         X, _ = self.p_x(z.cuda())
+#         return X
+
+    @staticmethod
+    def reparameterize(mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = torch.FloatTensor(std.size()).normal_().to(mu.device)
+        return eps.mul(std).add_(mu)
--- csvae_toy/train.py
+++ csvae_toy/train.py
@@ -18,16 +18,14 @@ from sklearn.datasets import make_swiss_roll
 from tqdm import tqdm, trange
 import matplotlib.pyplot as plt
 
-from definitions import VAE, CSVAE_without_delta_net
+from definitions import VAE, CSVAE_without_delta_net, CSVAE
 import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str)
+parser.add_argument('--model', type=str, choices=['VAE', 'CSVAE_without_delta_net', 'CSVAE'])
 parser.add_argument('--tag', type=str)
 args = parser.parse_args()
-
-assert args.model in ['VAE', 'CSVAE_without_delta_net']  # replace
 
 
 x_train, manifold_x_train = make_swiss_roll(n_samples=10000)
@@ -49,7 +47,8 @@ if args.model == 'VAE':
     
     test_set_tensor = torch.from_numpy(x_test).cuda()
     
-    model = VAE(input_dim, z_dim, w_dim, beta=1).train().cuda()
+    beta = 1
+    model = VAE(input_dim, z_dim, w_dim, beta=beta).train().cuda()
 elif args.model == 'CSVAE_without_delta_net':
     train_set_x_tensor = torch.from_numpy(x_train).cuda()
     train_set_y_tensor = torch.from_numpy(y_train).cuda()
@@ -60,55 +59,83 @@ elif args.model == 'CSVAE_without_delta_net':
     test_set_y_tensor = torch.from_numpy(y_test).cuda()
 
     model = CSVAE_without_delta_net(input_dim=x_train.shape[1], labels_dim=y_train.shape[1], z_dim=z_dim, w_dim=w_dim).train().cuda()
+elif args.model == 'CSVAE':
+    train_set_x_tensor = torch.from_numpy(x_train).cuda()
+    train_set_y_tensor = torch.from_numpy(y_train).cuda()
+    train_set = utils.TensorDataset(train_set_x_tensor, train_set_y_tensor)
+    train_loader = utils.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    
+    test_set_x_tensor = torch.from_numpy(x_test).cuda()
+    test_set_y_tensor = torch.from_numpy(y_test).cuda()
+
+    model = CSVAE(input_dim=x_train.shape[1], labels_dim=y_train.shape[1], z_dim=z_dim, w_dim=w_dim).train().cuda()
     
 opt = optim.Adam(model.parameters(), lr=1e-3/2)
 scheduler = optim.lr_scheduler.MultiStepLR(opt, milestones=[pow(3, i) for i in range(7)], gamma=pow(0.1, 1/7))
 n_epochs = 2300
 
 root_dir = 'res'
-png_dir = os.path.join(root_dir, args.tag, 'latent_vis')
+vis_dir = os.path.join(root_dir, args.tag, 'latent_vis')
 models_dir = os.path.join(root_dir, args.tag, 'models')
-os.makedirs(png_dir, exist_ok=True)
+os.makedirs(vis_dir, exist_ok=True)
 os.makedirs(models_dir, exist_ok=True)
 
 
-mse_losses = []
+x_recon_losses = []
 w_kl_losses = []
 z_kl_losses = []
+y_negentropy_losses = []
+y_recon_losses = []
 for epoch_i in trange(n_epochs):
     for cur_batch in train_loader:
         opt.zero_grad()
         if args.model == 'VAE':
             cur_batch = cur_batch[0]
             opt.zero_grad()
-            mse_loss_val, kl_loss_val, _ = model.calculate_loss(cur_batch)
-            loss_val = mse_loss_val + kl_loss_val*beta
+            x_recon_loss_val, kl_loss_val, _ = model.calculate_loss(cur_batch)
+            loss_val = x_recon_loss_val + kl_loss_val*beta
             loss_val.backward()
             opt.step()
-            mse_losses.append(mse_loss_val.item())
+            x_recon_losses.append(x_recon_loss_val.item())
             z_kl_losses.append((kl_loss_val*beta).item())
         elif args.model == 'CSVAE_without_delta_net':
-            cur_batch = cur_batch
-            loss_val, recon_loss_val, z_kl_loss_val, w_kl_loss_val = model.calculate_loss(*cur_batch)
+            loss_val, x_recon_loss_val, z_kl_loss_val, w_kl_loss_val = model.calculate_loss(*cur_batch)
             loss_val.backward()
             opt.step()
-            mse_losses.append(recon_loss_val.item())
+            x_recon_losses.append(x_recon_loss_val.item())
             z_kl_losses.append(z_kl_loss_val.item())
             w_kl_losses.append(w_kl_loss_val.item())
+        elif args.model == 'CSVAE':
+            loss_val, x_recon_loss_val, w_kl_loss_val, z_kl_loss_val, y_negentropy_loss_val, y_recon_loss_val = model.calculate_loss(*cur_batch)
+            loss_val.backward()
+            opt.step()
+            x_recon_losses.append(x_recon_loss_val.item())
+            w_kl_losses.append(w_kl_loss_val.item())
+            z_kl_losses.append(z_kl_loss_val.item())
+            y_negentropy_losses.append(y_negentropy_loss_val.item())
+            y_recon_losses.append(y_recon_loss_val.item())
     scheduler.step()
     print(f'Epoch {epoch_i}')
-    print(f'Mean MSE: {np.array(mse_losses[-len(train_loader):]).mean():.4f}')
+    print('Train')
+    print(f'MSE(x): {np.array(x_recon_losses[-len(train_loader):]).mean():.4f}')
     if args.model == 'VAE':
-        print(f'Mean KL: {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL:: {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
     elif args.model == 'CSVAE_without_delta_net':
-        print(f'Mean w KL: {np.array(w_kl_losses[-len(train_loader):]).mean():.4f}')
-        print(f'Mean z KL: {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(w): {np.array(w_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(z): {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+    elif args.model == 'CSVAE':
+        print(f'KL(w): {np.array(w_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(z): {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'-H(y): {np.array(y_negentropy_losses[-len(train_loader):]).mean():.4f}')
+        print(f'BCE(y): {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
     print()
     
     if args.model == 'VAE':
         _, _, zw, _, _ = model.forward(test_set_tensor)
     elif args.model == 'CSVAE_without_delta_net':
         _, _, zw, _, _, _, _, _, _ = model.forward(test_set_x_tensor, test_set_y_tensor)
+    elif args.model == 'CSVAE':
+        _, _, zw, _, _, _, _, _, _, _ = model.forward(test_set_x_tensor, test_set_y_tensor)
 
     colors_test = ['red' if label else 'blue' for label in y_test]
 
@@ -120,25 +147,25 @@ for epoch_i in trange(n_epochs):
     plt.figure(figsize=(5, 5,))
     plt.title(cur_title)
     plt.scatter(z_comp[:, 0], z_comp[:, 1], c=colors_test)
-    plt.savefig(os.path.join(png_dir, cur_title))
+    plt.savefig(os.path.join(vis_dir, cur_title))
 
     cur_title = f'(z2, w1), epoch {epoch_i}'
     plt.figure(figsize=(5, 5,))
     plt.title(cur_title)
     plt.scatter(z_comp[:, 1], w_comp[:, 0], c=colors_test)
-    plt.savefig(os.path.join(png_dir, cur_title))
+    plt.savefig(os.path.join(vis_dir, cur_title))
 
     cur_title = f'(w1, w2), epoch {epoch_i}'
     plt.figure(figsize=(5, 5,))
     plt.title(cur_title)
     plt.scatter(w_comp[:, 0], w_comp[:, 1], c=colors_test)
-    plt.savefig(os.path.join(png_dir, cur_title))
+    plt.savefig(os.path.join(vis_dir, cur_title))
 
     cur_title = f'(w2, z1), epoch {epoch_i}'
     plt.figure(figsize=(5, 5,))
     plt.title(cur_title)
     plt.scatter(w_comp[:, 1], w_comp[:, 0], c=colors_test)
-    plt.savefig(os.path.join(png_dir, cur_title))
+    plt.savefig(os.path.join(vis_dir, cur_title))
     
     plt.close('all')
     
