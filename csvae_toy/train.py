bfdbecdb595b885d9ec0052bd98ef3202e2e6187
nikkou <nikkou.postbox@gmail.com>
2019-10-24 09:14:22 +0300

    CSVAE: print test results, run with alternative params

--- csvae_toy/train.py
+++ csvae_toy/train.py
@@ -16,6 +16,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_swiss_roll
 from tqdm import tqdm, trange
+import dill
 import matplotlib.pyplot as plt
 
 from definitions import VAE, CSVAE_without_delta_net, CSVAE
@@ -48,7 +49,7 @@ if args.model == 'VAE':
     test_set_tensor = torch.from_numpy(x_test).cuda()
     
     beta = 1
-    model = VAE(input_dim, z_dim, w_dim, beta=beta).train().cuda()
+    model = VAE(x_train.shape[1], z_dim, w_dim, beta=beta).train().cuda()
 elif args.model == 'CSVAE_without_delta_net':
     train_set_x_tensor = torch.from_numpy(x_train).cuda()
     train_set_y_tensor = torch.from_numpy(y_train).cuda()
@@ -67,6 +68,8 @@ elif args.model == 'CSVAE':
     
     test_set_x_tensor = torch.from_numpy(x_test).cuda()
     test_set_y_tensor = torch.from_numpy(y_test).cuda()
+    test_set = utils.TensorDataset(test_set_x_tensor, test_set_y_tensor)
+    test_loader = utils.DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
     model = CSVAE(input_dim=x_train.shape[1], labels_dim=y_train.shape[1], z_dim=z_dim, w_dim=w_dim).train().cuda()
     
@@ -90,11 +93,20 @@ os.makedirs(vis_dir, exist_ok=True)
 os.makedirs(models_dir, exist_ok=True)
 
 
-x_recon_losses = []
-w_kl_losses = []
-z_kl_losses = []
-y_negentropy_losses = []
-y_recon_losses = []
+train_x_recon_losses = []
+train_w_kl_losses = []
+train_z_kl_losses = []
+train_y_negentropy_losses = []
+train_y_recon_losses = []
+
+test_x_recon_losses = []
+test_w_kl_losses = []
+test_z_kl_losses = []
+test_y_negentropy_losses = []
+test_y_recon_losses = []
+
+test_every = 100
+
 for epoch_i in trange(n_epochs):
     for cur_batch in train_loader:
         if args.model == 'VAE':
@@ -105,16 +117,16 @@ for epoch_i in trange(n_epochs):
             loss_val = x_recon_loss_val + kl_loss_val*beta
             loss_val.backward()
             opt.step()
-            x_recon_losses.append(x_recon_loss_val.item())
-            z_kl_losses.append((kl_loss_val*beta).item())
+            train_x_recon_losses.append(x_recon_loss_val.item())
+            train_z_kl_losses.append((kl_loss_val*beta).item())
         elif args.model == 'CSVAE_without_delta_net':
             opt.zero_grad()
             loss_val, x_recon_loss_val, z_kl_loss_val, w_kl_loss_val = model.calculate_loss(*cur_batch)
             loss_val.backward()
             opt.step()
-            x_recon_losses.append(x_recon_loss_val.item())
-            z_kl_losses.append(z_kl_loss_val.item())
-            w_kl_losses.append(w_kl_loss_val.item())
+            train_x_recon_losses.append(x_recon_loss_val.item())
+            train_z_kl_losses.append(z_kl_loss_val.item())
+            train_w_kl_losses.append(w_kl_loss_val.item())
         elif args.model == 'CSVAE':
             loss_val, x_recon_loss_val, w_kl_loss_val, z_kl_loss_val, y_negentropy_loss_val, y_recon_loss_val = model.calculate_loss(*cur_batch)
             
@@ -128,11 +140,11 @@ for epoch_i in trange(n_epochs):
             loss_val.backward()
             opt_without_delta.step()
         
-            x_recon_losses.append(x_recon_loss_val.item())
-            w_kl_losses.append(w_kl_loss_val.item())
-            z_kl_losses.append(z_kl_loss_val.item())
-            y_negentropy_losses.append(y_negentropy_loss_val.item())
-            y_recon_losses.append(y_recon_loss_val.item())
+            train_x_recon_losses.append(x_recon_loss_val.item())
+            train_w_kl_losses.append(w_kl_loss_val.item())
+            train_z_kl_losses.append(z_kl_loss_val.item())
+            train_y_negentropy_losses.append(y_negentropy_loss_val.item())
+            train_y_recon_losses.append(y_recon_loss_val.item())
     if args.model != 'CSVAE':
         scheduler.step()
     else:
@@ -140,57 +152,77 @@ for epoch_i in trange(n_epochs):
         scheduler_delta.step()
     print(f'Epoch {epoch_i}')
     print('Train')
-    print(f'MSE(x): {np.array(x_recon_losses[-len(train_loader):]).mean():.4f}')
+    print(f'MSE(x): {np.array(train_x_recon_losses[-len(train_loader):]).mean():.4f}')
     if args.model == 'VAE':
-        print(f'KL:: {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL:: {np.array(train_z_kl_losses[-len(train_loader):]).mean():.4f}')
     elif args.model == 'CSVAE_without_delta_net':
-        print(f'KL(w): {np.array(w_kl_losses[-len(train_loader):]).mean():.4f}')
-        print(f'KL(z): {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(w): {np.array(train_w_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(z): {np.array(train_z_kl_losses[-len(train_loader):]).mean():.4f}')
     elif args.model == 'CSVAE':
-        print(f'KL(w): {np.array(w_kl_losses[-len(train_loader):]).mean():.4f}')
-        print(f'KL(z): {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
-        print(f'-H(y): {np.array(y_negentropy_losses[-len(train_loader):]).mean():.4f}')
-        print(f'BCE(y): {np.array(z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(w): {np.array(train_w_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'KL(z): {np.array(train_z_kl_losses[-len(train_loader):]).mean():.4f}')
+        print(f'-H(y): {np.array(train_y_negentropy_losses[-len(train_loader):]).mean():.4f}')
+        print(f'BCE(y): {np.array(train_z_kl_losses[-len(train_loader):]).mean():.4f}')
     print()
+
+    torch.save(model.state_dict(), os.path.join(models_dir, 'model_{epoch_i}.pt'))
     
-    if args.model == 'VAE':
-        _, _, zw, _, _ = model.forward(test_set_tensor)
-    elif args.model == 'CSVAE_without_delta_net':
-        _, _, zw, _, _, _, _, _, _ = model.forward(test_set_x_tensor, test_set_y_tensor)
-    elif args.model == 'CSVAE':
-        _, _, zw, _, _, _, _, _, _, _ = model.forward(test_set_x_tensor, test_set_y_tensor)
+    if epoch_i % test_every == 0:
+        if args.model == 'CSVAE':
+            for cur_batch in test_loader:
+                loss_val, x_recon_loss_val, w_kl_loss_val, z_kl_loss_val, y_negentropy_loss_val, y_recon_loss_val = model.calculate_loss(*cur_batch)
 
-    colors_test = ['red' if label else 'blue' for label in y_test]
+                test_x_recon_losses.append(x_recon_loss_val.item())
+                test_w_kl_losses.append(w_kl_loss_val.item())
+                test_z_kl_losses.append(z_kl_loss_val.item())
+                test_y_negentropy_losses.append(y_negentropy_loss_val.item())
+                test_y_recon_losses.append(y_recon_loss_val.item())
 
-    zw = zw.detach().cpu().numpy()
-    z_comp = zw[:, :2]
-    w_comp = zw[:, 2:]
-    
-    cur_title = f'(z1, z2), epoch {epoch_i}'
-    plt.figure(figsize=(5, 5,))
-    plt.title(cur_title)
-    plt.scatter(z_comp[:, 0], z_comp[:, 1], c=colors_test)
-    plt.savefig(os.path.join(vis_dir, cur_title))
+            print('Test')
+            print(f'MSE(x): {np.array(test_x_recon_losses[-len(train_loader):]).mean():.4f}')
+            print(f'KL(w): {np.array(test_w_kl_losses[-len(train_loader):]).mean():.4f}')
+            print(f'KL(z): {np.array(test_z_kl_losses[-len(train_loader):]).mean():.4f}')
+            print(f'-H(y): {np.array(test_y_negentropy_losses[-len(train_loader):]).mean():.4f}')
+            print(f'BCE(y): {np.array(test_z_kl_losses[-len(train_loader):]).mean():.4f}')
 
-    cur_title = f'(z2, w1), epoch {epoch_i}'
-    plt.figure(figsize=(5, 5,))
-    plt.title(cur_title)
-    plt.scatter(z_comp[:, 1], w_comp[:, 0], c=colors_test)
-    plt.savefig(os.path.join(vis_dir, cur_title))
+        if args.model == 'VAE':
+            _, _, zw, _, _ = model.forward(test_set_tensor)
+        elif args.model == 'CSVAE_without_delta_net':
+            _, _, zw, _, _, _, _, _, _ = model.forward(test_set_x_tensor, test_set_y_tensor)
+        elif args.model == 'CSVAE':
+            _, _, zw, _, _, _, _, _, _, _ = model.forward(test_set_x_tensor, test_set_y_tensor)
 
-    cur_title = f'(w1, w2), epoch {epoch_i}'
-    plt.figure(figsize=(5, 5,))
-    plt.title(cur_title)
-    plt.scatter(w_comp[:, 0], w_comp[:, 1], c=colors_test)
-    plt.savefig(os.path.join(vis_dir, cur_title))
+        colors_test = ['red' if label else 'blue' for label in y_test]
 
-    cur_title = f'(w2, z1), epoch {epoch_i}'
-    plt.figure(figsize=(5, 5,))
-    plt.title(cur_title)
-    plt.scatter(w_comp[:, 1], w_comp[:, 0], c=colors_test)
-    plt.savefig(os.path.join(vis_dir, cur_title))
-    
-    plt.close('all')
-    
-    torch.save(model, os.path.join(models_dir, 'vae.pt'))  # may slow down training if arch is large!
+        zw = zw.detach().cpu().numpy()
+        z_comp = zw[:, :2]
+        w_comp = zw[:, 2:]
+
+        scatter_size = 12
+
+        cur_title = f'(z1, z2), epoch {epoch_i}'
+        plt.figure(figsize=(5, 5,))
+        plt.title(cur_title)
+        plt.scatter(z_comp[:, 0], z_comp[:, 1], c=colors_test, s=scatter_size)
+        plt.savefig(os.path.join(vis_dir, cur_title))
+
+        cur_title = f'(z2, w1), epoch {epoch_i}'
+        plt.figure(figsize=(5, 5,))
+        plt.title(cur_title)
+        plt.scatter(z_comp[:, 1], w_comp[:, 0], c=colors_test, s=scatter_size)
+        plt.savefig(os.path.join(vis_dir, cur_title))
+
+        cur_title = f'(w1, w2), epoch {epoch_i}'
+        plt.figure(figsize=(5, 5,))
+        plt.title(cur_title)
+        plt.scatter(w_comp[:, 0], w_comp[:, 1], c=colors_test, s=scatter_size)
+        plt.savefig(os.path.join(vis_dir, cur_title))
+
+        cur_title = f'(w2, z1), epoch {epoch_i}'
+        plt.figure(figsize=(5, 5,))
+        plt.title(cur_title)
+        plt.scatter(w_comp[:, 1], w_comp[:, 0], c=colors_test, s=scatter_size)
+        plt.savefig(os.path.join(vis_dir, cur_title))
+
+        plt.close('all')
     
